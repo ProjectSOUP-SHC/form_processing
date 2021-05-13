ef0c79c6bcf0daf277ade9622291bd5f17308ed4
Christian Bayer <christian.bayer@salesforce.com>
2021-05-13 16:53:51 -0400

    packages init

--- init.R
+++ init.R
@@ -1,5 +1,5 @@
 ##############################################
 ######### install dependencies ###############
 ##############################################
-
+install.packages("remotes")
 remotes::install_github("paulc91/shinyauthr")
