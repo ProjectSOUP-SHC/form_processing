ce1d2a5d735fffb95af52a2e643368f67b9d4354
Alissa E <aebel@shcinc.org>
2021-05-14 12:56:37 -0400

    start of first/second viist processing - still needs work

--- FoodSelection.R
+++ FoodSelection.R
@@ -2,7 +2,7 @@
 
 ####### Enter Delivery Phone Numbers Here ########
 
-dels <- c('781-333-0754')
+dels <- c('617-230-0464', '617-599-2915')
 
 ##################################################
 
@@ -10,10 +10,10 @@ library(tidyverse)
 library(Hmisc)
 library(googlesheets4)
 
-qnames <- read_csv('data_clean/pickup_match.csv')
+qnames <- read_csv('data_clean/pickup_match_eng.csv')
 guest.data.raw <- read_csv('data_clean/visit_history_current.csv')
 guest.data.clean <- guest.data.raw %>%
-  select(id = "Guest ID", first = "First Name", middle = "Middle Name", last = "Last Name", hhsize = "Total Individuals", visit = "Visit on") %>%
+  select(id = "Guest ID", first = "First Name", middle = "Middle Name", last = "Last Name", hhsize = "Total Individuals", visit = "Visit on", phone = "Home Phone") %>%
   separate(visit, into=c("visit.date", "visit.time"), sep = " ") %>%
   mutate(visit.date = as.Date(visit.date, "%Y-%m-%d"),
          yearmon = substring(as.character(visit.date), first = 1, last = 7),
@@ -36,7 +36,7 @@ guest.final <- guest.data.clean %>%
   group_by(id, yearmon, hhsize, name.sb) %>%
   summarise(visits = n()) %>%
   ungroup() %>%
-  mutate(visit_no = ifelse(yearmon=='2021-04', visits, 0)) %>%
+  mutate(visit_no = ifelse(yearmon=='2021-05', visits, 0)) %>%
   group_by(id, hhsize, name.sb) %>%
   summarise(visits = sum(visit_no))
 
@@ -45,23 +45,27 @@ rm(guest.size)
 
 day.trans <- read_csv('data_clean/day_trans.csv', locale = readr::locale(encoding = "latin1"))
 
-langs <- c('Resp_Eng', 'Resp_KA', 'Resp_Port', 'Resp_Esp')
+langs <- c('Resp_Eng2', 'Resp_KA2', 'Resp_Esp2', 'Resp_Port2')
+
 
 for (l in langs){
   
   resp <- read_sheet('https://docs.google.com/spreadsheets/d/1m9bbVTLoEgN2Ne8Ns1jgW-f-R7NOw_RANsRDxqX9HQ8/edit#gid=553182362',
                      sheet = l)
-    
-  names(resp) <- qnames$q_short
+  names(resp) <- qnames$q_short_eng
+  
   
   resp.fin <- resp %>%
     mutate(time = as.Date(substring(as.character(time), first = 1, last = 10)),
-           allergies = as.character(allergies)
+           allergies2 = as.character(allergies2),
+           allergies1 = as.character(allergies1),
+           milk1 = ifelse(is.list(milk1), unlist(milk1), milk1),
+           milk2 = ifelse(is.list(milk2), unlist(milk2), milk2)
            ) %>%
     filter(time>Sys.Date()-6|del=="Yes"|del=="Si"|del=="Wi"|del=="Sí") %>%
     merge(day.trans, by.x = "pickup", by.y = "pickup", all.x = T) %>%
-    filter(str_detect(pickup_eng, pattern = paste(weekdays(Sys.Date())))|phone %in% dels) %>%
-    mutate(milk = unlist(milk))
+    filter(str_detect(pickup_eng, pattern = paste(weekdays(Sys.Date())))|phone %in% dels|first_nm %in% dels)
+    
   
   if (nrow(resp.fin)==0) next
   
@@ -74,6 +78,7 @@ for (l in langs){
   }
   
 }
+
 
 rm(l, langs, resp, resp.fin)
 
@@ -102,7 +107,7 @@ orders.fin <- orders.fin %>%
   merge(name.of, by.x = "name", by.y = "orders.fin.name", all.x = T) %>%
   merge(guest.final, by = "name.sb", all.x = T) %>%  
   mutate(pickup_eng = ifelse(is.na(pickup_eng), "Delivery", pickup_eng),
-         visit_no = ifelse(visits==0, "First",
+         visit_no = ifelse(visits==0|name=="Victoria Mahin", "First",
                            ifelse(visits==1, "Second", "ThirdPlus")))
 
 rm(name.of, day.trans, orders)
@@ -110,30 +115,56 @@ rm(name.of, day.trans, orders)
 
 # Create google sheet for Save as Doc processing
 
-out_frs <- orders.fin %>%
-  select(Name = name, Size = hhsize, Visit = visit_no, Pickup = pickup_eng, Restrictions = allergies,
-         FreshVeg = veg_frs, FreshFruit = fruit_frs, 
-         Milk = milk, Eggs = eggs, Cheese = cheese, Yogurt = yogurt,
-         FrozenProduce = produce_frz, FrozenProtein = protein_frz) %>%
+
+out_frs1 <- orders.fin %>%
+  filter(visit_no=="First"|is.na(visit_no)) %>%
+  select(Name = name, Size = hhsize, Visit = visit_no, Pickup = pickup_eng, Restrictions = allergies1,
+         FreshVeg = veg_frs1, FreshFruit = fruit_frs1, 
+         Milk = milk1, Eggs = eggs1, Cheese = cheese1, Yogurt = yogurt1,
+         FrozenProduce = produce_frz1, FrozenProtein = protein_frz1) %>%
+  t() %>%
+  as.data.frame() %>%
+  bind_cols(item = row.names(.)) %>%
+  relocate(item)
+
+out_frs2 <- orders.fin %>%
+  filter(visit_no=="Second") %>%
+  select(Name = name, Size = hhsize, Visit = visit_no, Pickup = pickup_eng, Restrictions = allergies2,
+         FreshVeg = veg_frs2, FreshFruit = fruit_frs2, 
+         FrozenProduce = produce_frz2, FrozenProtein = protein_frz2) %>%
   t() %>%
   as.data.frame() %>%
   bind_cols(item = row.names(.)) %>%
   relocate(item)
 
 
-out_dry <- orders.fin %>%
-  select(Name = name, Size = hhsize, Visit = visit_no, Phone = phone, Pickup = pickup_eng, Restrictions = allergies,
-         Cereal = cereal, CanMeat = protein_cnd, CanFruit = fruit_cnd, CanVeg = veg_cnd,
-         Juice = juice, Soup = soup, Tomato = tom, DryFruit = dry_fruit,
-         Pasta = pasta, Rice = rice, EasyPrep = mac,
-         PeanutButter = pb, Beans = beans, Milk = milk, Condiment = con,
-         Oil = oil, Coffee = coffee, Toiletry = toil) %>%
+
+out_dry1 <- orders.fin %>%
+  filter(visit_no=="First"|is.na(visit_no)) %>%
+  select(Name = name, Size = hhsize, Visit = visit_no, Phone = phone, Pickup = pickup_eng, Restrictions = allergies1,
+         Cereal = cereal1, CanMeat = protein_cnd1, CanFruit = fruit_cnd1, CanVeg = veg_cnd1,
+         Juice = juice1, Soup = soup1, Tomato = tom1, DryFruit = dry_fruit1,
+         Pasta = pasta1, Rice = rice1, EasyPrep = mac1,
+         PeanutButter = pb1, Beans = breans1, Milk = milk1, Condiment = con1,
+         Oil = oil1, Coffee = coffee1, Toiletry = toil1) %>%
   t() %>%
   as.data.frame() %>%
   bind_cols(item = row.names(.)) %>%
   relocate(item)
 
-gs4_create(paste("Orders_", Sys.Date(), sep = ''), sheets = list(dry = out_dry, fresh = out_frs))
+out_dry2 <- orders.fin %>%
+  filter(visit_no=="Second") %>%
+  select(Name = name, Size = hhsize, Visit = visit_no, Phone = phone, Pickup = pickup_eng, Restrictions = allergies2,
+         Cereal = cereal2, CanMeat = protein_cnd2, CanFruit = fruit_cnd2, CanVeg = veg_cnd2,
+         Juice = juice2, Soup = soup2, Tomato = tom2, DryFruit = dry_fruit2,
+         Pasta = pasta2, Rice = rice2, EasyPrep = mac2,
+         PeanutButter = pb2, Beans = beans2, Milk = milk2) %>%
+  t() %>%
+  as.data.frame() %>%
+  bind_cols(item = row.names(.)) %>%
+  relocate(item)
+
+gs4_create(paste("Orders_", Sys.Date()+1, "_Del", sep = ''), sheets = list(dry1 = out_dry1, fresh1 = out_frs1, dry2 = out_dry2, fresh2 = out_frs2))
 
 
 
