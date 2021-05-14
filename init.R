91c22c8ce88a5d6fa535a03bad49645ab2043dd9
christian <christian@t530>
2021-05-13 20:47:50 -0400

    added tidyverse

--- init.R
+++ init.R
@@ -2,4 +2,5 @@
 ######### install dependencies ###############
 ##############################################
 install.packages("remotes")
+install.packages("tidyverse")
 remotes::install_github("paulc91/shinyauthr")
