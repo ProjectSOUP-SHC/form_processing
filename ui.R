3ff8b567e7fa8d6860bd926aedd5d8af5fa1d8d8
christian <christian@t530>
2021-06-03 22:34:09 -0400

    added keepalive for heroku

--- ui.R
+++ ui.R
@@ -2,27 +2,37 @@
 ################# Shiny App UI ###############
 ##############################################
 
-#library(dplyr)
 library(shiny)
 library(shinyjs)        # improve user experience with JavaScript
 library(shinyauthr)
-# library(shinymanager)   # shiny authentication modules
-# library(sodium)
 
 ui <- fluidPage(
   # must turn shinyjs on
   shinyjs::useShinyjs(),
-  # add logout button UI 
+  # add logout button UI
   div(class = "pull-right", logoutUI(id = "logout")),
   # add login panel UI function
   loginUI(id = "login"),
   
   textInput("dels", "Phone number for delivery"),
   actionButton("generate", "Generate spreadsheet"),
-  mainPanel(fluidRow(
-    htmlOutput("frame")
-  )
-  )
+  mainPanel(fluidRow(htmlOutput("frame"))),
+  tags$head(
+    HTML(
+      "
+          <script>
+          var socket_timeout_interval
+          var n = 0
+          $(document).on('shiny:connected', function(event) {
+          socket_timeout_interval = setInterval(function(){
+          Shiny.onInputChange('count', n++)
+          }, 15000)
+          });
+          $(document).on('shiny:disconnected', function(event) {
+          clearInterval(socket_timeout_interval)
+          });
+          </script>
+          "
+    )
+  ),
 )
-
-# shinyApp(ui = ui, server = server)
